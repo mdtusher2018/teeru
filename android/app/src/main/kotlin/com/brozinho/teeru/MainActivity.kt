545e17dc80621339cf0d2b4c9ce66d9347f8c53f
siamprodhan <siambci@gmail.com>
2025-06-09 15:20:21 +0600

    initial commit

---
+++ android/app/src/main/kotlin/com/brozinho/teeru/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.brozinho.teeru
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity : FlutterFragmentActivity()

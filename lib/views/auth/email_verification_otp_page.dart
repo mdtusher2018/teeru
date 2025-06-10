16c03bcf897ef99e72f3dd2f0b7c6c1843cdfa04
Shakil Sheikh <www.shakil.sheikh222@gmail.com>
2025-06-10 08:46:56 +0600

    localization

--- lib/views/auth/email_verification_otp_page.dart
+++ lib/views/auth/email_verification_otp_page.dart
@@ -23,80 +23,82 @@ class EmailVerifiedOTPScreen extends StatelessWidget {
         leading: BackButton(),
       ),
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            commonText("OTP Verification".tr, size: 18, isBold: true),
-            SizedBox(height: 8),
-            SizedBox(
-              width: 200,
-              child: commonText(
-                "We’ve sent a 6-digit code to your email/phone number.\nPlease enter the code below to continue."
-                    .tr,
-                size: 14,
-                textAlign: TextAlign.center,
-                color: AppColors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              commonText("OTP Verification".tr, size: 18, isBold: true),
+              SizedBox(height: 8),
+              SizedBox(
+                width: 200,
+                child: commonText(
+                  "We’ve sent a 6-digit code to your email/phone number.\nPlease enter the code below to continue."
+                      .tr,
+                  size: 14,
+                  textAlign: TextAlign.center,
+                  color: AppColors.white,
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(
-                controller.otpControllers.length,
-                (index) => Padding(
-                  padding: EdgeInsets.only(right: 4),
-                  child: buildOTPTextField(
-                    controller.otpControllers[index],
-                    index,
-                    context,
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  controller.otpControllers.length,
+                  (index) => Padding(
+                    padding: EdgeInsets.only(right: 4),
+                    child: buildOTPTextField(
+                      controller.otpControllers[index],
+                      index,
+                      context,
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 16),
+              SizedBox(height: 16),
 
-            commonButton(
-              controller.isLoading.value ? "Verifying...".tr : "Verify".tr,
-              onTap:
-                  controller.isLoading.value
-                      ? null
-                      : () async {
-                        await controller.verifyOtp();
-                      },
-            ),
-            SizedBox(height: 16),
-            SizedBox(
-              width: MediaQuery.sizeOf(context).width * 0.8,
-              child: Wrap(
-                alignment: WrapAlignment.center,
-                spacing: 20,
-                children: [
-                  commonText("Didn’t get the code?".tr, isBold: true),
-                  GestureDetector(
-                    onTap: () {
-                      Get.to(SignUpScreen());
-                    },
-                    child: commonText(
-                      "Resend".tr,
-                      fontWeight: FontWeight.bold,
-                      color: AppColors.white,
-                    ),
-                  ),
-
-                  GestureDetector(
-                    onTap: () {
-                      Get.back();
-                    },
-                    child: commonText(
-                      "Cancel".tr,
-                      fontWeight: FontWeight.bold,
-                      color: AppColors.white,
-                    ),
-                  ),
-                ],
+              commonButton(
+                controller.isLoading.value ? "Verifying...".tr : "Verify".tr,
+                onTap:
+                    controller.isLoading.value
+                        ? null
+                        : () async {
+                          await controller.verifyOtp();
+                        },
               ),
-            ),
-          ],
+              SizedBox(height: 16),
+              SizedBox(
+                width: MediaQuery.sizeOf(context).width * 0.8,
+                child: Wrap(
+                  alignment: WrapAlignment.center,
+                  spacing: 20,
+                  children: [
+                    commonText("Didn’t get the code?".tr, isBold: true),
+                    GestureDetector(
+                      onTap: () {
+                        Get.to(SignUpScreen());
+                      },
+                      child: commonText(
+                        "Resend".tr,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.white,
+                      ),
+                    ),
+
+                    GestureDetector(
+                      onTap: () {
+                        Get.back();
+                      },
+                      child: commonText(
+                        "Cancel".tr,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/views/auth/forget_password.dart
+++ lib/views/auth/forget_password.dart
@@ -17,60 +17,62 @@ class ForgetPasswordScreen extends StatelessWidget {
       appBar: AppBar(leading: BackButton()),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            SizedBox(height: 20),
-            Column(
-              children: [
-                commonText("Forgot Password".tr, size: 16, isBold: true),
-                SizedBox(height: 8),
-                SizedBox(
-                  width: MediaQuery.sizeOf(context).width * 0.7,
-                  child: commonText(
-                    "Enter email or phone number to send one time OTP Verification code"
-                        .tr,
-                    textAlign: TextAlign.center,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(height: 20),
+              Column(
+                children: [
+                  commonText("Forgot Password".tr, size: 16, isBold: true),
+                  SizedBox(height: 8),
+                  SizedBox(
+                    width: MediaQuery.sizeOf(context).width * 0.7,
+                    child: commonText(
+                      "Enter email or phone number to send one time OTP Verification code"
+                          .tr,
+                      textAlign: TextAlign.center,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 12),
+                  const SizedBox(height: 12),
 
-                // Use controller's emailController
-                commonTextfield(
-                  controller.emailController,
-                  hintText: "Enter Email or Phone Number".tr,
-                ),
-
-                const SizedBox(height: 20),
-
-                Obx(
-                  () => commonButton(
-                    controller.isLoading.value
-                        ? "Sending...".tr
-                        : "Generate OTP".tr,
-                    height: 40,
-                    onTap:
-                        controller.isLoading.value
-                            ? null
-                            : () async {
-                              await controller.sendOtp();
-                            },
+                  // Use controller's emailController
+                  commonTextfield(
+                    controller.emailController,
+                    hintText: "Enter Email or Phone Number".tr,
                   ),
-                ),
 
-                const SizedBox(height: 12),
-                InkWell(
-                  onTap: () {
-                    Get.back();
-                  },
-                  child: commonText("Cancel".tr, size: 18, isBold: true),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Image.asset("assets/images/full_logo.png"),
-            SizedBox(height: 20),
-          ],
+                  const SizedBox(height: 20),
+
+                  Obx(
+                    () => commonButton(
+                      controller.isLoading.value
+                          ? "Sending...".tr
+                          : "Generate OTP".tr,
+                      height: 40,
+                      onTap:
+                          controller.isLoading.value
+                              ? null
+                              : () async {
+                                await controller.sendOtp();
+                              },
+                    ),
+                  ),
+
+                  const SizedBox(height: 12),
+                  InkWell(
+                    onTap: () {
+                      Get.back();
+                    },
+                    child: commonText("Cancel".tr, size: 18, isBold: true),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Image.asset("assets/images/full_logo.png"),
+              SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
